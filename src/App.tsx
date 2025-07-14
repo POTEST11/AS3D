76b0abd4512130ac6ea6c919b90ada9cb82bfc89
AndresManjarres <andreswin99@gmail.com>
2025-07-13 19:55:29 -0500

    Sidebar-Dashboard 1st version

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
+<<<<<<< HEAD
 
 import './App.css'
+=======
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+>>>>>>> e20dda1 (Sidebar-Dashboard 1st version)
 
 function App() {
-
-  if (false) {
-
   return (
-    <>
-    <h1>
-      AS3D - A Simple 3D Engine
-    </h1>
-
- 
-    </>
-  )
-}
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
 }
 
-export default App
+export default App;
