00df45c90c49e4b81e49ce580c3ef535a3c119c2
AndresManjarres <andreswin99@gmail.com>
2025-07-18 14:21:39 -0500

    React Router Aplicado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
